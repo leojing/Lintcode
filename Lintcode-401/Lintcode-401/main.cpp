40bcbf46289b96a5f1f897bbc7f3a4cfafdf3081
JINGLUO <leojing1989@gmail.com>
2019-07-04 23:38:50 +1000

    401. Kth Smallest Number in Sorted Matrix

--- Lintcode-401/Lintcode-401/main.cpp
+++ Lintcode-401/Lintcode-401/main.cpp
@@ -11,61 +11,99 @@
 #include <set>
 #include <tuple>
 
-void visitMatrix(std::vector<std::vector<int>> &matrix, int k, std::priority_queue<int> &pq, std::set<std::tuple<int, int>> &visited, int x, int y) {
+class Node {
+public:
+    int val, X, Y;
+    Node(int v, int x, int y) {
+        val = v;
+        X = x;
+        Y = y;
+    }
+};
+
+struct CustomCompare
+{
+    bool operator()(const Node& lhs, const Node& rhs)
+    {
+        return lhs.val > rhs.val; // priority_queue 这个比大小是反过来的，如果希望最小的在top，那么left > right
+    }
+};
+
+int visitMatrix(std::vector<std::vector<int>> &matrix, int k, std::priority_queue<Node, std::vector<Node>, CustomCompare> &pq, std::set<std::tuple<int, int>> &visited) {
     int row = int(matrix.size());
     int colum = int(matrix[0].size());
-    if (x < row && y < colum) {
-        std::tuple<int, int> t = std::make_tuple(x,y);
-        if (visited.find(t) != visited.end()) {
-            return;
-        }
-        if (pq.size() < k) {
-            pq.push(matrix[x][y]);
-        } else {
-            int top = pq.top();
-            if (top > matrix[x][y]) {
-                pq.pop();
-                pq.push(matrix[x][y]);
+    pq.push(Node(matrix[0][0], 0, 0)); // 现将0，0的点放入，这个肯定是最小的
+//    k --;
+    while (--k) {
+        Node top = pq.top();
+        pq.pop();
+        if (top.X + 1 < row) { // 保证没有越界
+            std::tuple<int, int> t = std::make_tuple(top.X + 1,top.Y);
+            if (visited.find(t) == visited.end()) { // 保证只放入没有找过的点，找过的点就不需要重复放入了
+                pq.push(Node(matrix[top.X + 1][top.Y], top.X + 1, top.Y));
+                visited.insert(t);
             }
         }
-        visited.insert(t);
-        visitMatrix(matrix, k, pq, visited, x+1, y);
-        visitMatrix(matrix, k, pq, visited, x, y+1);
+        if (top.Y + 1 < colum) { // 保证没有越界
+            std::tuple<int, int> t = std::make_tuple(top.X,top.Y + 1);
+            if (visited.find(t) == visited.end()) { // 保证只放入没有找过的点，找过的点就不需要重复放入了
+                pq.push(Node(matrix[top.X][top.Y + 1], top.X, top.Y + 1));
+                visited.insert(t);
+            }
+        }
     }
-    return;
+    Node result = pq.top();
+    return result.val;
 }
 
 int kthSmallest(std::vector<std::vector<int>> &matrix, int k) {
     if (matrix.size() == 0) {
         return 0;
     }
-    std::priority_queue<int> pq;
+    std::priority_queue<Node, std::vector<Node>, CustomCompare> pq;
     std::set<std::tuple<int, int>> visited;
-    visitMatrix(matrix, k, pq, visited, 0, 0);
-    return pq.top();
+    return visitMatrix(matrix, k, pq, visited);
 }
 
 int main(int argc, const char * argv[]) {
     std::vector<std::vector<int>> matrix;
     std::vector<int> row1, row2, row3;
-    row1.push_back(532);
-    row1.push_back(22);
-    row1.push_back(42);
-    row1.push_back(5462);
+    row1.push_back(1);
+    row2.push_back(2);
+    row3.push_back(3);
+    row1.push_back(4);
 
-    row2.push_back(24);
-    row2.push_back(232);
-    row2.push_back(432);
-    row2.push_back(682);
+    row3.push_back(5);
+    row2.push_back(6);
+    row1.push_back(7);
+    row2.push_back(8);
 
-    row3.push_back(8752);
-    row3.push_back(243);
-    row3.push_back(254);
-    row3.push_back(345442);
+    row1.push_back(9);
+    row2.push_back(10);
+    row3.push_back(11);
+    row3.push_back(12);
 
-//    matrix.push_back(row1);
-//    matrix.push_back(row2);
-//    matrix.push_back(row3);
+    matrix.push_back(row1);
+    matrix.push_back(row2);
+    matrix.push_back(row3);
     std::cout << kthSmallest(matrix, 10) << "\n";
     return 0;
 }
+
+
+/*
+###算法
+模拟题，不能用DFS，否则会TLE
+
+###代码实现
+***有什么要注意的地方
+参考注释代码
+***有什么要优化的地方
+
+###时空复杂度分析
+Time O(KlogT)，查找K次，每次进行（一次pq.pop()=logT，两次pq.push()=2logT）,T为pq的大小，每次删减操作需要的时间为logT
+Space O(n)，n为pq的大小，也是n is the maximum of the width and height of the matrix.
+
+###相关题目
+Leetcode 988. Smallest String Starting From Leaf
+*/
