f7311b53aac3aa84ca21418ffb913a3531204e9d
JINGLUO <leojing1989@gmail.com>
2019-08-04 17:23:22 +1000

    Add 707

---
+++ Lintcode-707/Lintcode-707/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Lintcode-707
+//
+//  Created by Jing LUO on 29/7/19.
+//  Copyright © 2019 Jing LUO. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
